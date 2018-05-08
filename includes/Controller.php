6a617feb9404e7d2cfb4c548c1dbe9f0d3cabf07
docker <dock3r@gmail.com>
2018-05-08 11:14:24 +0200

    add styles to related product buttton

--- includes/Controller.php
+++ includes/Controller.php
@@ -6,9 +6,13 @@ class Controller
 
 	function __construct()
 	{
+		$this->plugin_path = plugin_dir_path(dirname(__FILE__));
+		$this->plugin_url = plugin_dir_url(dirname(__FILE__));
+
 		$this->acf = new ACF();
 
 		add_filter('the_content', [$this, 'insertAfterContent']);
+		add_action('wp_enqueue_scripts', [$this, 'registerScripts']);
 	}
 
 	public function insertAfterContent($content)
@@ -17,5 +21,10 @@ class Controller
 		$related_products = $view->render('related_products');
 		$content .= $related_products;
 		return $content;
+	}
+
+	public function registerScripts()
+	{
+		wp_enqueue_style( 'cncep-style' , $this->plugin_url . 'assets/style.css');
 	}
 }
