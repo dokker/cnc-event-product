028307365a44565581090d59c2e572697868a60a
docker <dock3r@gmail.com>
2018-05-08 10:59:01 +0200

    add view layer and related template

--- includes/Controller.php
+++ includes/Controller.php
@@ -6,5 +6,16 @@ class Controller
 
 	function __construct()
 	{
+		$this->acf = new ACF();
+
+		add_filter('the_content', [$this, 'insertAfterContent']);
+	}
+
+	public function insertAfterContent($content)
+	{
+		$view = new \cncEP\View();
+		$related_products = $view->render('related_products');
+		$content .= $related_products;
+		return $content;
 	}
 }
