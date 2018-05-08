2dde59b70b0bfca3203c962e91e80134fd228924
docker <dock3r@gmail.com>
2018-05-08 10:58:25 +0200

    field base settings

---
+++ includes/ACF.php
@@ -0,0 +1,66 @@
+<?php
+namespace cncEP;
+
+class ACF
+{
+
+	function __construct()
+	{
+		$this->registerFields();
+	}
+
+	public function registerFields()
+	{
+		if( function_exists('acf_add_local_field_group') ):
+
+		acf_add_local_field_group(array (
+			'key' => 'group_58b02a1e3a1d5',
+			'title' => 'Kapcsolódó termék',
+			'fields' => array (
+				array (
+					'key' => 'field_58b039bd88490',
+					'label' => 'Kapcsolódó termék',
+					'name' => 'related_product',
+					'type' => 'post_object',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array (
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'post_type' => array (
+						0 => 'product',
+					),
+					'taxonomy' => array (
+						0 => 'product_cat:esemenyek',
+					),
+					'allow_null' => 1,
+					'multiple' => 0,
+					'return_format' => 'object',
+					'ui' => 1,
+				),
+			),
+			'location' => array (
+				array (
+					array (
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'event',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => 1,
+			'description' => '',
+		));
+
+		endif;
+	}
+}
